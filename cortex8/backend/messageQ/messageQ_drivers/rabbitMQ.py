eae944219ff51045f9b8d48d212041de9370b8a8
amitshaikoren <amitshaik@mail.tau.ac.il>
2020-08-04 16:11:22 +0300

    implemented rabbitMQ.py

--- cortex8/backend/messageQ/messageQ_drivers/rabbitMQ.py
+++ cortex8/backend/messageQ/messageQ_drivers/rabbitMQ.py
@@ -1,7 +1,7 @@
 import pika
 
 
-class rabbitMQ:
+class RabbitMQ:
     def __init__(self, host, port):
         self.host = host
         self.port = port
@@ -10,12 +10,31 @@ class rabbitMQ:
 
     def publish(self, body):
         connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.host, port=self.port))
-        channel = self.connection.channel()
+        channel = connection.channel()
         channel.queue_declare(queue=self.name)
+
         channel.basic_publish(exchange='',
                               routing_key=self.name,
                               body=body)
+
         connection.close()
 
     def consume(self):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.host, port=self.port))
+        channel = connection.channel()
+        channel.queue_declare(queue=self.name)
 
+        def callback(ch, method, properties, body):
+            print(" [x] Received %r" % body)
+
+        channel.basic_consume(queue=self.name,
+                              auto_ack=True,
+                              on_message_callback=callback)
+
+        channel.start_consuming()
+
+
+if __name__ == "__main__":
+    mq = RabbitMQ(host="localhost", port="5672")
+    mq.consume()
+    mq.publish("hello")
